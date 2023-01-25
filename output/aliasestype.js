cb6d8e522fc76786a8cebf427472a689f6cd099d
Ashik045 <ashikurislam045@gmail.com>
2023-01-25 10:34:34 +0600

    function signature and class

--- output/aliasestype.js
+++ output/aliasestype.js
@@ -3,3 +3,10 @@ const userDetails = (id, user) => {
     console.log(`userId ${id}, userName ${user.name}, age ${user.age}`);
 };
 userDetails(20232, { name: "Ashik", age: 22 });
+// *************** Lesson 8 *******************************
+// Function Signature (define a function structure)
+let addTwoNumbers;
+addTwoNumbers = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(addTwoNumbers(4, 4));
--- src/aliasestype.ts
+++ src/aliasestype.ts
@@ -8,3 +8,13 @@ const userDetails = (id: stringOrNumber, user: userInfo) => {
   console.log(`userId ${id}, userName ${user.name}, age ${user.age}`);
 };
 userDetails(20232, { name: "Ashik", age: 22 });
+
+// *************** Lesson 8 *******************************
+// Function Signature (define a function structure)
+let addTwoNumbers: (num1: number, num2: number) => number;
+
+addTwoNumbers = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+console.log(addTwoNumbers(4, 4));
